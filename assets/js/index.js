0f3f7d57da221618903665757b56145b5c16e01e
Victor Costa <victorcostaguitar@gmail.com>
2014-07-07 17:42:58 -0300

    add browser

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('deviceready', onDeviceReady, true);
 
 function onDeviceReady() {	
-	alert('opas!');
+	ref = window.open(encodeURI('http://facebook.com'), '_self', 'location=no');
 }
