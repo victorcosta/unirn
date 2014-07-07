8bc0b3068f5e3b1c3e814d3654ba916661a95948
Victor Costa <victorcostaguitar@gmail.com>
2014-07-07 10:34:12 -0300

    some changes

--- js/index.js
+++ js/index.js
@@ -34,6 +34,8 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        alert('opa');
+        
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
