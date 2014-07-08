310b24432e9698f73d890ef574a71b6b44ee304d
Victor Costa <victorcostaguitar@gmail.com>
2014-07-08 09:48:33 -0300

    adf

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,30 +5,27 @@ function onDeviceReady() {
 		openFacebook();
 	});
 	$('#alert').click(function(event) {
-		showAlert();
-		//alert('teste!');
+		navigator.notification.alert(
+            'You are the winner!',  // message
+            alertDismissed,         // callback
+            'Game Over',            // title
+            'Done'                  // buttonName
+            );
+		alert('.');
 	});
 	$('#vibrate').click(function(event) {
-		vibrate();
+		navigator.notification.vibrate(2000);
+		alert('.');
 	});
 	$('#beep').click(function(event) {
-		playBeep();
+		navigator.notification.beep(3);
+		alert('.');
 	});
 
 }
 function openFacebook() {	
 	window.open(encodeURI('http://facebook.com'), '_self', 'location=no');
 }
-function showAlert() {
-	navigator.notification.alert(
-            'You are the winner!',  // message
-            'Game Over',            // title
-            'Done'                  // buttonName
-            );
-}
-function vibrate() {
-	navigator.notification.vibrate(2000);
-}
-function playBeep() {
-	navigator.notification.beep(3);
-}
+function alertDismissed() {
+        // do something
+    }
