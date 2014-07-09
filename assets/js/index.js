494b4f3513e9ee346d7e4af868ca5b72fc68935d
Victor Costa <victorcostaguitar@gmail.com>
2014-07-09 09:47:34 -0300

    test

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,8 +14,8 @@ function onDeviceReady() {
 		navigator.notification.beep(3);   
 	});
 	$('#beep').click(function(event) {
-		navigator.notification.beep(5);   
-		navigator.notification.vibrate(1000);
+		navigator.notification.beep(3);   
+		navigator.notification.vibrate(2000);
 		navigator.notification.alert("Hello",null,"hahahahA",'i`m fine');
 	});
 
