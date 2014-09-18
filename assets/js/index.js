b1ea6671cb16c8afbba86b67b043134a56295a3e
Victor Costa <victorcostaguitar@gmail.com>
2014-09-18 10:18:20 -0300

    removendo paginas de teste

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,6 @@
 document.addEventListener('deviceready', onDeviceReady, true);
 
 function onDeviceReady() {
-	var conection = checkConnection();
-	// $('#alert').click(function(event) {
-	// 	navigator.notification.alert("Hello");
-	// });
-	// $('#vibrate').click(function(event) {
-	// 	navigator.notification.vibrate(1000);
-	// });
-	// $('#beep').click(function(event) {
-	// 	navigator.notification.beep(3);
-	// });
-	// $('#all').click(function(event) {
-	// 	navigator.notification.beep(1);
-	// 	navigator.notification.vibrate(2000);
-	// 	navigator.notification.alert("Hello",null,"hahahahA",'i`m fine');
-	// });
-
-	navigator.notification.alert(conection,null,"Alerta",'OK!');
-
 	$("#btn-acessar").click(function(event) {
 		var matricula 	= $('#matricula').val();
 		var cpf 		= $('#cpf').val();
@@ -29,10 +11,7 @@ function onDeviceReady() {
 			var ref = window.open(encodeURI('http://projetos.maxmeio.com/unirn_app/?matricula='+matricula+'&cpf='+cpf), '_self', 'location=no');
 		};
 	});
-
-	// checaConexao();
-
-
+	var ref = checkConnection();
 }
 
 function checkConnection() {
