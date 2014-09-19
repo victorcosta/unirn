8a554875deb4dbcd4192df18d57f4d16ed7a4374
Victor Costa <victorcostaguitar@gmail.com>
2014-09-19 14:52:26 -0300

    tentando colocar o localstorage

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,10 +9,15 @@ function onDeviceReady() {
 			navigator.notification.vibrate(100);
 			navigator.notification.alert("Digite a matrícula e o CPF",null,"Alerta",'OK!');
 		}else{
+			window.localStorage.clear();
+			window.localStorage.matricula 	= matricula;
+			window.localStorage.cpf 		= cpf;
+
 			var ref = window.open(encodeURI('http://projetos.maxmeio.com/unirn_app/?matricula='+matricula+'&cpf='+cpf), '_self', 'location=no');
 		};
 	});
 	checkConection();
+	getDataForm();
 }
 function onOffLine() {
 	navigator.notification.vibrate(100);
@@ -25,21 +30,11 @@ function closeApp(){
 		navigator.device.exitApp();
 	}
 }
-
-
-// function checaConexao(){
-// 	var networkState = navigator.connection.type;
-// 	// if (networkState == 'Connection.NONE') {
-// 	navigator.notification.alert(networkState,null,"Alerta",'OK!');
-// 	if (networkState != 'Connection.WIFI') {
-// 		navigator.notification.vibrate(100);
-// 		navigator.notification.alert("Você está sem conexão com a rede",null,"Alerta",'OK!');
-
-// 		if (navigator.app) {
-//            navigator.app.exitApp();
-//         }
-//         else if (navigator.device) {
-//             navigator.device.exitApp();
-//         }
-// 	};
-// }
+function getDataForm(){
+	if (window.localStorage.matricula) {
+		$('#matricula').val(window.localStorage.matricula);
+	};
+	if (window.localStorage.cpf) {
+		$('#matricula').val(window.localStorage.cpf);
+	};
+}
