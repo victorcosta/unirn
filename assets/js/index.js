872fd1d496186bad5de8bb195c00368ae4c3066d
Victor Costa <victorcostaguitar@gmail.com>
2014-09-19 15:10:04 -0300

    removendo localstorage novamente

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,15 +9,10 @@ function onDeviceReady() {
 			navigator.notification.vibrate(100);
 			navigator.notification.alert("Digite a matrícula e o CPF",null,"Alerta",'OK!');
 		}else{
-			window.localStorage.clear();
-			window.localStorage.matricula 	= matricula;
-			window.localStorage.cpf 		= cpf;
-
 			var ref = window.open(encodeURI('http://projetos.maxmeio.com/unirn_app/?matricula='+matricula+'&cpf='+cpf), '_self', 'location=no');
 		};
 	});
 	checkConection();
-	getDataForm();
 }
 function onOffLine() {
 	navigator.notification.vibrate(100);
@@ -29,12 +24,4 @@ function closeApp(){
 	}else if (navigator.device) {
 		navigator.device.exitApp();
 	}
-}
-function getDataForm(){
-	if (window.localStorage.matricula) {
-		$('#matricula').val(window.localStorage.matricula);
-	};
-	if (window.localStorage.cpf) {
-		$('#matricula').val(window.localStorage.cpf);
-	};
 }
