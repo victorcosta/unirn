9ddd7ec87b991941d20ab06104db26600f7b943c
Victor Costa <victorcostaguitar@gmail.com>
2015-02-09 11:27:58 -0300

    Trying push notification

--- assets/js/index.js
+++ assets/js/index.js
@@ -56,7 +56,7 @@ function onDeviceReady() {
 
 	try {
 		pushNotification = window.plugins.pushNotification;
-		if (device.platform == 'android' || device.platform == 'Android') {
+		if (navigator.device.platform == 'android' || navigator.device.platform == 'Android') {
 			pushNotification.register(successHandler, errorHandler, {"senderID":"510667245122","ecb":"onNotificationGCM"});		// required!
 		} else {
 			pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
